75a7d69bf34cd3edbdcb5772eb071d84267af7a4
timothyshores <timothymshores@gmail.com>
2023-07-12 18:50:39 -0600

    Style navbar component

--- React Info Site/src/Components/Navbar.jsx
+++ React Info Site/src/Components/Navbar.jsx
@@ -1,3 +1,16 @@
-const Navbar = () => <h1>Navbar</h1>;
+import Image from "../assets/react-icon-small.png";
+import "./Navbar.css";
+
+const Navbar = () => (
+	<nav>
+		<div className="left">
+			<img src={Image} alt="React logo" />
+			<h2>React Facts</h2>
+		</div>
+		<div className="right">
+			<h3>React Course - Project 1</h3>
+		</div>
+	</nav>
+);
 
 export default Navbar;
