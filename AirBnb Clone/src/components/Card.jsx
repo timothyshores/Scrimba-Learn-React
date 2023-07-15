a3a3fb8555e88b10135bcc397bb75058920cfb70
timothyshores <timothymshores@gmail.com>
2023-07-15 16:57:56 -0600

    Create card component

--- AirBnb Clone/src/components/Card.jsx
+++ AirBnb Clone/src/components/Card.jsx
@@ -1,25 +1,56 @@
-import Swimmer from "../images/swimmer.png";
-import Star from "../images/star.png";
+/* eslint-disable no-unused-vars */
+
+import PropTypes from "prop-types";
 import "./Card.css";
 
-const Card = () => (
-	<div className="card">
-		<img
-			src={Swimmer}
-			alt="Female professional swimmer"
-			className="card-image"
-		/>
-		<div className="info">
-			<img src={Star} alt="Star icon" className="star-icon" />
-			<span>5.0</span>
-			<span className="gray">(6) • </span>
-			<span className="gray">USA </span>
+const Card = ({
+	title,
+	description,
+	price,
+	coverImg,
+	stats,
+	location,
+	openSpots,
+}) => {
+	const { rating, reviewCount } = stats;
+	const imageLocation = `../../public/images/${coverImg}`;
+
+	return (
+		<div className="card">
+			<img
+				src={imageLocation}
+				className="card-image"
+			/>
+			<div className="info">
+				<img
+					src="../../public/images/star.png"
+					alt="Star icon"
+					className="star-icon"
+				/>
+				<span>{rating}</span>
+				<span className="gray">({reviewCount}) • </span>
+				<span className="gray">{location}</span>
+			</div>
+			<p className="card--title">{title}</p>
+			<p className="card--price">
+				<span className="bold">From ${price}</span> / person
+			</p>
 		</div>
-		<p>Life Lessons with Katie Zaferes</p>
-		<p>
-			<span className="bold">From $136</span> / person
-		</p>
-	</div>
-);
+	);
+};
+
+Card.propTypes = {
+	id: PropTypes.number.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string,
+	price: PropTypes.number.isRequired,
+	coverImg: PropTypes.string.isRequired,
+	stats: PropTypes.shape({
+		rating: PropTypes.number.isRequired,
+		reviewCount: PropTypes.number.isRequired,
+	}).isRequired,
+	location: PropTypes.string.isRequired,
+	openSpots: PropTypes.number,
+};
 
 export default Card;
