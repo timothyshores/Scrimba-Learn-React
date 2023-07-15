3cd34889825e6e75696b1257ba5a28b12622aec1
timothyshores <timothymshores@gmail.com>
2023-07-14 23:12:51 -0600

    Create new react app using vite

---
+++ AirBnb Clone/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+	return (
+		<>
+			<h1>AirBnb Clone</h1>
+		</>
+	);
+};
+
+export default App;
--- React Info Site/src/Components/Main.jsx
+++ React Info Site/src/Components/Main.jsx
@@ -1,3 +1,19 @@
-const Main = () => <h1>Main</h1>;
+import "./Main.css";
+
+const Main = () => (
+	<div className="container">
+		<h1>Fun Facts about React</h1>
+		<ul>
+			<li>Was first released in 2013</li>
+			<li>Was originally created by Jordan Walke</li>
+			<li>Has well over 100K stars on GitHub</li>
+			<li>Is maintained by Facebook</li>
+			<li>
+				<p>Powers thousands of enterprise apps, including </p>
+				<p>mobile apps</p>
+			</li>
+		</ul>
+	</div>
+);
 
 export default Main;
