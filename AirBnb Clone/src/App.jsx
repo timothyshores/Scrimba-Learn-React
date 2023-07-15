e9ae7f0f6f2c74c34d0741e51800d8108bfc80b4
timothyshores <timothymshores@gmail.com>
2023-07-15 16:58:56 -0600

    Import card into App.jsx and render Card components using data file

--- AirBnb Clone/src/App.jsx
+++ AirBnb Clone/src/App.jsx
@@ -1,16 +1,40 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import cardDataArray from "./data";
+
 import "./App.css";
 
-const App = () => {
-	return (
-		<>
-			<Navbar />
-			<Hero />
-			<Card />
-		</>
-	);
-};
+const App = () => (
+	<>
+		<Navbar />
+		<Hero />
+		<div className="cards-container">
+			{cardDataArray.map(
+				({
+					id,
+					title,
+					description,
+					price,
+					coverImg,
+					stats,
+					location,
+					openSpots,
+				}) => (
+					<Card
+						key={id}
+						title={title}
+						description={description}
+						price={price}
+						coverImg={coverImg}
+						stats={stats}
+						location={location}
+						openSpots={openSpots}
+					/>
+				)
+			)}
+		</div>
+	</>
+);
 
 export default App;
