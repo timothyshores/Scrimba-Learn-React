bb4f6a9c52e7a3e4ca5a4e22116fa64a39ebcad6
timothyshores <timothymshores@gmail.com>
2023-07-16 05:12:48 -0600

    Create form input UI

--- meme-generator/src/App.jsx
+++ meme-generator/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Meme from "./components/Meme";
 import "./App.css";
 
 const App = () => (
 	<>
 		<Header />
+		<Meme />
 	</>
 );
 
---
+++ meme-generator/src/components/Meme.jsx
@@ -0,0 +1,13 @@
+import "./Meme.css";
+
+const Meme = () => (
+	<main>
+		<form className="form">
+			<input type="text" placeholder="Top text" className="form--input" />
+			<input type="text" placeholder="Bottom text" className="form--input" />
+			<button className="form--button">Get a new meme image 🖼</button>
+		</form>
+	</main>
+);
+
+export default Meme;
