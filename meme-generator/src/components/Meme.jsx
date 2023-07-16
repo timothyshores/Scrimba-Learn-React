aecf351fd33bfcaaa0d1afb672958b72b23ab519
timothyshores <timothymshores@gmail.com>
2023-07-16 13:46:10 -0600

    Add feature console log a random meme url when button is clicked

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,13 +1,25 @@
+import results from "../data.js";
 import "./Meme.css";
 
-const Meme = () => (
-	<main>
-		<form className="form">
-			<input type="text" placeholder="Top text" className="form--input" />
-			<input type="text" placeholder="Bottom text" className="form--input" />
-			<button className="form--button">Get a new meme image 🖼</button>
-		</form>
-	</main>
-);
+const Meme = () => {
+	const handleClick = () => {
+		const { memes: memesArray } = results.data;
+		const randomIndex = Math.floor(Math.random() * memesArray.length);
+		const { url: randomMemeUrl } = memesArray[randomIndex];
+		console.log(randomMemeUrl);
+	};
+
+	return (
+		<main>
+			<div className="form">
+				<input type="text" placeholder="Top text" className="form--input" />
+				<input type="text" placeholder="Bottom text" className="form--input" />
+				<button className="form--button" onClick={handleClick}>
+					Get a new meme image 🖼
+				</button>
+			</div>
+		</main>
+	);
+};
 
 export default Meme;
