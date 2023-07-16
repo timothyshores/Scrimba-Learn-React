8f4f1d891fc00cbe02bc590c058462a1919165ea
timothyshores <timothymshores@gmail.com>
2023-07-16 17:29:16 -0600

    Click button and get random meme image from local data.js file

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import results from "../data.js";
 import "./Meme.css";
 
 const Meme = () => {
+	const [memeImage, setMemeImage] = useState("");
+
 	const handleClick = () => {
 		const { memes: memesArray } = results.data;
 		const randomIndex = Math.floor(Math.random() * memesArray.length);
 		const { url: randomMemeUrl } = memesArray[randomIndex];
-		console.log(randomMemeUrl);
+		setMemeImage(randomMemeUrl);
 	};
 
 	return (
@@ -17,6 +20,13 @@ const Meme = () => {
 				<button className="form--button" onClick={handleClick}>
 					Get a new meme image 🖼
 				</button>
+				{memeImage && (
+					<img
+						className="meme-image"
+						src={memeImage}
+						alt="meme image from meme generator"
+					/>
+				)}
 			</div>
 		</main>
 	);
